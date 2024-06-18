d6367084bc2145dee71b224708aa4ee41ebd7175
abhijeetlokhande1996 <abhijeetlokhande1996@gmail.com>
2024-06-18 21:37:21 +0100

    Train Reformatted

--- train.py
+++ train.py
@@ -1,16 +1,11 @@
 import mlx.core
-import mlx.nn
 import torch
 import numpy as np
-
 from pathlib import Path
 import mlx.core as mx
 from model import Bert, load_model
 from datasets import DatasetDict, Dataset
 import mlx
-# from mlx.nn.losses import cross_entropy
-import mlx.core as mx
-import mlx.nn as nn
 from mlx.optimizers import Adam
 from tqdm import tqdm
 import time
@@ -28,8 +23,8 @@ class ModelForTokenClassification(nn.Module):
     def __init__(self, num_labels, max_length=512) -> None:
         super().__init__()
         self.bert_model, self.bert_tokenizer = load_model(
-            "bert-base-uncased",
-            "weights/bert-base-uncased.npz")
+            "bert-base-uncased", "weights/bert-base-uncased.npz"
+        )
         self.bert_model.freeze()
         self.bert_model_output_dim = 768
         self.dropout = nn.Dropout(0.1)
@@ -60,7 +55,8 @@ def compute_loss(logits: mx.array, y: mx.array) -> float:
     valid_logits = logits[mask]
     valid_labels = y[mask]
     loss = mlx.nn.losses.cross_entropy(
-        mx.array(valid_logits, dtype=mx.bfloat16), mx.array(valid_labels)).mean()
+        mx.array(valid_logits, dtype=mx.bfloat16), mx.array(valid_labels)
+    ).mean()
     # et = time.time()
     # print(" Compute Loss :: ", (et-st), " seconds")
     return loss
@@ -84,23 +80,23 @@ def loss_function(model, x: mx.array, y: mx.array) -> mx.array:
     return compute_loss(logits, y)
 
 
-def get_validation_loss_and_metric(model: ModelForTokenClassification, validation_dataset: Dataset) -> Dict:
+def get_validation_loss_and_metric(
+    model: ModelForTokenClassification, validation_dataset: Dataset
+) -> Dict:
     print("**** Computing Validation Loss ****")
     model.eval()
     total_validation_loss = 0
     for i in range(0, len(validation_dataset), batch_size):
-        test_slice = validation_dataset[i: i + batch_size]
+        test_slice = validation_dataset[i : i + batch_size]
         test_slice.pop("words")
         test_slice.pop("ner_labels")
 
         test_labels = test_slice.pop("labels")
         test_labels = mx.array([mx.array(array) for array in test_labels])
         for key in test_slice.keys():
-            test_slice[key] = mx.array(
-                [mx.array(array) for array in test_slice[key]])
+            test_slice[key] = mx.array([mx.array(array) for array in test_slice[key]])
 
-        total_validation_loss += loss_function(model,
-                                               test_slice, test_labels).tolist()
+        total_validation_loss += loss_function(model, test_slice, test_labels).tolist()
 
     return total_validation_loss
 
@@ -112,26 +108,27 @@ if __name__ == "__main__":
     train_dataset = Dataset.load_from_disk("./hf_train_ner_dataset")
 
     validation_dataset = Dataset.load_from_disk("./hf_test_ner_dataset")
-    model: ModelForTokenClassification = ModelForTokenClassification(
-        num_labels=21)
+    model: ModelForTokenClassification = ModelForTokenClassification(num_labels=21)
 
     loss_and_grad_fn = nn.value_and_grad(model, loss_function)
 
     optimizer = mlx.optimizers.Adam(learning_rate=5e-5)
     batch_size = 32
     num_epochs = 1
-    epoch_iterator = tqdm(iterable=range(num_epochs),
-                          total=num_epochs, desc="Epochs")
+    epoch_iterator = tqdm(iterable=range(num_epochs), total=num_epochs, desc="Epochs")
     for epoch in epoch_iterator:
         print("Epoch :: ", epoch, " Processing!")
         train_loss_for_epoch = 0
         validation_loss_for_epoch = 0
-        train_batch_iterator = tqdm(range(0, len(train_dataset), batch_size), total=len(
-            train_dataset)//batch_size, desc="Batches")
+        train_batch_iterator = tqdm(
+            range(0, len(train_dataset), batch_size),
+            total=len(train_dataset) // batch_size,
+            desc="Batches",
+        )
         model.train()
         model.bert_model.freeze()
         for i in train_batch_iterator:
-            slice = train_dataset[i: i + batch_size]
+            slice = train_dataset[i : i + batch_size]
 
             if "words" in slice.keys():
                 slice.pop("words")
@@ -140,11 +137,16 @@ if __name__ == "__main__":
 
             labels = slice.pop("labels")
             for key in slice.keys():
-                slice[key] = mx.array([mx.array(array,)
-                                      for array in slice[key]])
+                slice[key] = mx.array(
+                    [
+                        mx.array(
+                            array,
+                        )
+                        for array in slice[key]
+                    ]
+                )
 
-            labels = mx.array([mx.array(array)
-                              for array in labels])
+            labels = mx.array([mx.array(array) for array in labels])
             loss_value, grads = loss_and_grad_fn(model, slice, labels)
             optimizer.update(model, grads)
             train_loss_for_epoch += loss_value.tolist()
