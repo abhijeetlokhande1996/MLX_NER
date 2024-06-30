e79d9c55c4b8e916b5dceccd4601b54f2a70ea78
abhijeetlokhande1996 <abhijeetlokhande1996@gmail.com>
2024-06-30 15:37:11 +0100

    Question replaced

--- phi3_test.py
+++ phi3_test.py
@@ -3,7 +3,7 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 model = AutoModelForCausalLM.from_pretrained("microsoft/Phi-3-mini-4k-instruct")
 tokenizer = AutoTokenizer.from_pretrained("microsoft/Phi-3-mini-4k-instruct")
 
-messages = [{"role": "user", "content": "Can you provide ways to eat combinations of bananas and dragonfruits?"}]
+messages = [{"role": "user", "content": "Extract Location, Name and output it in JSON format. My name is Sarah and I live in London"}]
 inputs = tokenizer.apply_chat_template(messages, add_generation_prompt=True, return_tensors="pt")
 
 outputs = model.generate(inputs, max_new_tokens=32)
